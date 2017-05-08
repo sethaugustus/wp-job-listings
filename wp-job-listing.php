a1b43c765fa1b0a54c3ca86f43ab4c42b6bcf251
Seth Quittner <sethaugustus@gmail.com>
2017-05-08 11:07:19 -0700

    broke file up into sub fiels and created an inc directory

--- wp-job-listing.php
+++ wp-job-listing.php
@@ -13,60 +13,6 @@ if (! defined( 'ABSPATH' ) ) {
   exit;
 }
 
-function sqd_register_jobs_post_type(){
-  $singular = 'Job Listing';
-  $plural = 'Job Listings';
-  $slug = str_replace( ' ', '_', strtolower( $singular ) );
+$dir = plugin_dir_path(__FILE__);
+require ( $dir . 'inc/wp-jobs-sqd.php');
 
-  $labels = array(
-    'name'                => $plural,
-    'singular_name'       => $singular,
-    'add_new'             => 'Add New',
-    'add_new_item'        => 'Add New ' . $singular,
-    'edit'                => 'Edit',
-    'edit_item'           => 'Edit ' . $singular,
-    'new_item'            => 'New ' . $singular,
-    'view'                => 'View ' . $singular,
-    'view_item'           => 'View ' . $singular,
-    'search_term'         => 'Search ' . $plural,
-    'parent'              => 'Parent ' . $singular,
-    'not_found'           => 'No ' . $plural .' found',
-    'not_found_in_trash'  => 'No ' . $plural .' in Trash'
-    );
-
-  $args = array(
-      'labels'              => $labels,
-            'public'              => true,
-            'publicly_queryable'  => true,
-            'exclude_from_search' => false,
-            'show_in_nav_menus'   => true,
-            'show_ui'             => true,
-            'show_in_menu'        => true,
-            'show_in_admin_bar'   => true,
-            'menu_position'       => 10,
-            'menu_icon'           => 'dashicons-businessman',
-            'can_export'          => true,
-            'delete_with_user'    => false,
-            'hierarchical'        => false,
-            'has_archive'         => true,
-            'query_var'           => true,
-            'capability_type'     => 'post',
-            'map_meta_cap'        => true,
-            // 'capabilities' => array(),
-            'rewrite'             => array( 
-              'slug'        => $slug,
-              'with_front'  => true,
-              'pages'       => true,
-              'feeds'       => true,
-            ),
-            'supports'            => array( 
-              'title', 
-              'editor', 
-              'author', 
-              'custom-fields' 
-            )
-    );
-  register_post_type( $slug, $args );
-}
-
-add_action( 'init', 'sqd_register_jobs_post_type' );
