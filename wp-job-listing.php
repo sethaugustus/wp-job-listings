4601b46d707a281a56e8e2298bfe465601169a10
Seth Quittner <sethaugustus@gmail.com>
2017-05-07 19:04:12 -0700

    set up plugin template

---
+++ wp-job-listing.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Plugin Name: WP Job Listings
+* Plugin URI: http://sethquittner.com
+* Description: A test plugin
+* Author: Seth Quittner
+* Author URI: http://sethquittner.com
+* Version: 1.0.0
+* License: GPLv2
+*/
+//exit if accessed directly
+if (! defined( 'ABSPATH' ) ) {
+  exit;
+}
